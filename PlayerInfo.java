fb6d7b35907520828eaac6aaebe088b87dc32b38
Pianocraft1YT <lucca.chen@crpusd.org>
2025-10-14 10:00:06 -0700

    health updates

--- PlayerInfo.java
+++ PlayerInfo.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class PlayerInfo {
     public static void main(String[] args) {
-
+        public static void increaseHealth(this.currentHealth)
+        System.out.println("Your current health is: " + currentHealth);
+        currentHealth+=1;
+        System.out.println("You healed for one health!")
     }
 }
