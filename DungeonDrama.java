4a1b9113bf04d5a33cc3f5d21ed1310d616af13e
Nicholas Todhunter <nicholas.todhunter@crpusd.org>
2025-10-14 10:33:05 -0700

    Completing mian psuedo, needs player and room

--- DungeonDrama.java
+++ DungeonDrama.java
@@ -7,10 +7,10 @@ public class DungeonDrama {
         String SEED = name;
         //Create Player Object with name, full health and starting inventory
         System.out.println("You find yourself in a circular room, the brick is mossy and cracked. The only breaks in the wall are three doors: 
-                            on the left a large wrot Iron door, with rusted hinges(1), 
-                            on the right a small Stone door that blends into the wall around it(2) 
-                            and in the middle a plain red wooden door, one would find in a nice house(3) /n
-                            which door do you choose?");
+                            on your left a large wrot iron door, with rusted hinges(1), 
+                            on the right a small stone door that blends into the wall around it(2) 
+                            and in the middle a plain red wooden door, with mold speckling the edges.(3) /n
+                            Which door do you choose?");
         /*while(player.alive) {
             if(player.keys < 3) {
             room = new Rooms(SEED);
