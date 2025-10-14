4903fe68d161a447eb7bf847f3f747993daac79b
Nicholas Todhunter <nicholas.todhunter@crpusd.org>
2025-10-14 09:47:50 -0700

    started work on main

--- DungeonDrama.java
+++ DungeonDrama.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class DungeonDrama {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name adventure!");
+        String name = scanner.nextLine();
+        //Create Player Object with name, full health and starting inventory
+        System.out.println("You find yourself in a circular room with three doors  ")
+
     }
 }
