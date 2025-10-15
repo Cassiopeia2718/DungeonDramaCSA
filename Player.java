9e685c0555440f3160e67e6cd9268fc25806adc0
Pianocraft1YT <lucca.chen@crpusd.org>
2025-10-15 09:14:28 -0700

    changed currenthealth to health

--- Player.java
+++ Player.java
@@ -4,25 +4,31 @@ public class Player {
     public static void main(String[] args) {
         int keys = 0;
         boolean alive = true;
-        int currentHealth = 3;
-        public static increaseHealth(this.currentHealth){
-        if (currentHealth >= 3){
+        int health = 3;
+        public static increaseHealth(this.health){
+        if (health >= 3){
             System.out.println("You are already max health!")
         }
         else{
-        System.out.println("Your current health is: " + currentHealth);
-        currentHealth+=1;
+        System.out.println("Your current health is: " + health);
+        health+=1;
         System.out.println("You healed for one health!")
-        return currentHealth;
+        return health;
         }
         }
     }
-    public static alive(this.currentHealth){
-        if (currentHealth<=0){
+    public static alive(this.health){
+        if (health<=0){
             System.out.println("You died with " + player.keys + " keys!")
             alive = false;
-            return dead;
+            return alive;
         }
         }
-    public static void giveKey()
+    public static giveKey(this.keys){
+        if (keys <3){
+            keys+=1;
+            System.out.println("You now have "+ keys + " keys.")
+        }
+    }
+
 }
