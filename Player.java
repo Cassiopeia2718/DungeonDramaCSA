0c570624b37e97c641f6f22800fcc938bde3972c
Pianocraft1YT <lucca.chen@crpusd.org>
2025-10-15 09:43:35 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,34 +1,43 @@
 import java.util.Scanner;
 public class Player {
     
-    public static void main(String[] args) {
-        int keys = 0;
-        boolean alive = true;
-        int health = 3;
-        public static increaseHealth(this.health){
+        private int keys = 0;
+        private boolean alive = true;
+        private int health = 3;
+
+        public void increaseHealth() { 
         if (health >= 3){
-            System.out.println("You are already max health!")
+            System.out.println("You are already max health!");
         }
         else{
         System.out.println("Your current health is: " + health);
-        health+=1;
-        System.out.println("You healed for one health!")
-        return health;
+        this.health+=1;
+        System.out.println("You healed for one health!");
         }
-        }
-    }
-    public static alive(this.health){
-        if (health<=0){
-            System.out.println("You died with " + player.keys + " keys!")
-            alive = false;
-            return alive;
-        }
-        }
-    public static giveKey(this.keys){
-        if (keys <3){
-            keys+=1;
-            System.out.println("You now have "+ keys + " keys.")
         }
     }
 
+
+public void setHealth(){
+    health = 3;
+    System.out.println("Your health starts at " + health ".")
 }
+  
+
+ public int getHealth() {
+    return this.health;
+    System.out.println("You have " + health + " health.")
+ }
+ public void alive(){
+        if (health<=0){
+            System.out.println("You died with " + this.keys + " keys!");
+            alive = false;
+        }
+        }
+    public void giveKey(){
+        if (keys < 3){
+            keys+=1;
+            System.out.println("You now have "+ this.keys + " keys.")
+        }
+    }
+
