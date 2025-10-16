66467c16411ab61f8ee9e7ccde7aaf3797d88926
Nicholas Todhunter <nicholas.todhunter@crpusd.org>
2025-10-16 08:46:29 -0700

    Refromated Player

--- Player.java
+++ Player.java
@@ -6,38 +6,38 @@ public class Player {
         private int health = 3;
 
         public void increaseHealth() { 
-        if (health >= 3){
-            System.out.println("You are already max health!");
+            if (health >= 3){
+                System.out.println("You are already max health!");
+            }
+            else{
+            this.health+=1;
+            System.out.println("Your health is now: " + this.health);
+            }
         }
-        else{
-        System.out.println("Your current health is: " + health);
-        this.health+=1;
-        System.out.println("You healed for one health!");
+
+        public void decreaseHealth() {     
+            this.health-=1;
         }
+        
+        public int getHealth() {
+            return this.health;
         }
+
+        public boolean alive(){
+            return this.alive        
+        }
+
+        public void check_death(){
+            if (health<=0){
+                alive = false;
+            }
+        }
+
+        public void giveKey(){
+            if (keys < 3){
+                keys+=1;
+                System.out.println("You now have "+ this.keys + " keys.")
+            }
+        }
+        
     }
-
-
-public void setHealth(){
-    health = 3;
-    System.out.println("Your health starts at " + health ".")
-}
-  
-
- public int getHealth() {
-    return this.health;
-    System.out.println("You have " + health + " health.")
- }
- public void alive(){
-        if (health<=0){
-            System.out.println("You died with " + this.keys + " keys!");
-            alive = false;
-        }
-        }
-    public void giveKey(){
-        if (keys < 3){
-            keys+=1;
-            System.out.println("You now have "+ this.keys + " keys.")
-        }
-    }
-
