73786808dcdcdd8288255aad5ccc9ea442fd3340
Nicholas Todhunter <nicholas.todhunter@crpusd.org>
2025-10-15 19:05:07 -0700

    Main and Rooms

--- DungeonDrama.java
+++ DungeonDrama.java
@@ -6,7 +6,7 @@ public class DungeonDrama {
         String name = scanner.nextLine();
         String SEED = name;
         //Create Player Object with name, full health and starting inventory
-        System.out.println("You find yourself in a circular room, the brick is mossy and cracked. The only breaks in the wall are three doors: 
+        System.out.println("You find yourself in a circular room, the brick is mossy and cracked. The only exits are three doors: 
                             on your left a large wrot iron door, with rusted hinges(1), 
                             on the right a small stone door that blends into the wall around it(2) 
                             and in the middle a plain red wooden door, with mold speckling the edges.(3) /n
